a0a6c038d84c803ee60ba14bd5cf53636fca7dfe
Rinto Jose <rintoj@gmail.com>
2024-06-13 17:23:16 +0530

    make write fail proof

--- src/process-document.ts
+++ src/process-document.ts
@@ -12,25 +12,33 @@ export async function processDocument(document: vscode.TextDocument) {
   }
   const lines = output.split('\n')
   const original = content.split('\n')
-  const numberOfLines = Math.max(lines.length, original.length)
+  const numberOfLines = Math.max(lines?.length, original?.length) ?? 0
   vscode.window.activeTextEditor?.edit(builder => {
     const doc = vscode.window.activeTextEditor?.document
     if (!doc) {
       return
     }
-    for (let line = 0; line < numberOfLines; line++) {
-      if (!lines[line] && line < original.length) {
-        console.log('delete', line)
-        const { range } = doc.lineAt(line)
-        builder.delete(range)
-      } else if (line < original.length && lines[line] !== original[line]) {
-        console.log('replace', line)
-        const { range } = doc.lineAt(line)
-        builder.replace(range, lines[line])
-      } else if (line >= original.length) {
-        console.log('insert', line)
-        builder.insert(new vscode.Position(line, 0), lines[line] + '\n')
+    try {
+      for (let line = 0; line < numberOfLines; line++) {
+        if (!lines[line] && line < original?.length) {
+          console.log('delete', line)
+          const { range } = doc.lineAt(line)
+          builder.delete(range)
+        } else if (line < original?.length && lines[line] !== original[line]) {
+          console.log('replace', line)
+          const { range } = doc.lineAt(line)
+          builder.replace(range, lines[line])
+        } else if (line >= original?.length) {
+          console.log('insert', line)
+          builder.insert(new vscode.Position(line, 0), lines[line] + '\r')
+        }
       }
+    } catch (e) {
+      console.warn(e)
+      builder.replace(
+        new vscode.Range(doc.lineAt(0).range.start, doc.lineAt(doc.lineCount - 1).range.end),
+        output,
+      )
     }
   })
 }
